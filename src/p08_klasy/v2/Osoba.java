933f61154ed0bdd2ff4195042b0e3d72e8e385c1
Patryk Czarnik <patryk@patrykczarnik.net>
2020-02-11 09:39:18 +0100

    v1

---
+++ src/p08_klasy/v2/Osoba.java
@@ -0,0 +1,18 @@
+package p08_klasy.v2;
+
+public class Osoba {
+	// zmienne definiowane w klasie (bez słowa static) nazywa się:
+	// * pole (field)
+	// * atrybut (attribute)
+	// * zmienna instancyjna (instance variable)
+	String imie, nazwisko;
+	int wiek;
+	
+	// metody, czyli czynności, które będą mogłby wykonać obiekty tej klasy
+	// metoda bez "static" jest metodą instancyjną, czyli wyobrażamy sobie, że to obiekty wykonuje metodę
+	void przedstawSie() {
+		System.out.println("Nazywam się " + imie + " " + nazwisko + " i mam " + wiek + "lat.");
+	}
+	
+
+}
