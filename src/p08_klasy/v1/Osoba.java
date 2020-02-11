b1c6d2385f812d37f8c0ea13a89fedb0f8a7068c
Patryk Czarnik <patryk@patrykczarnik.net>
2020-02-11 09:34:02 +0100

    zmienne instancyjne

---
+++ src/p08_klasy/v1/Osoba.java
@@ -0,0 +1,11 @@
+package p08_klasy.v1;
+
+public class Osoba {
+	// zmienne definiowane w klasie (bez słowa static) nazywa się:
+	// * pole (field)
+	// * atrybut (attribute)
+	// * zmienna instancyjna (instance variable)
+	String imie, nazwisko;
+	int wiek;
+
+}
